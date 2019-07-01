ede6c19d98f25175a19eed5211818d71d8742640
naufalafif <nafualafif58@gmail.com>
2019-07-01 11:33:19 +0700

    init css

--- src/App.vue
+++ src/App.vue
@@ -16,4 +16,46 @@ export default {
 </script>
 
 <style>
+.cart {
+    width: 400px;
+    margin: auto;
+  }
+
+.cart-head {
+  padding: 10px;
+  display: flex;
+}
+
+.cart-head .info {
+  flex:1;
+}
+
+.cart-head .add-cart {
+  flex:1;
+}
+
+.cart-item {
+  display: flex;
+  padding: 10px;
+}
+
+.cart-item .title {
+  flex:1;
+  font-size: 26px;
+  font-weight: bold;
+}
+.cart-item span {
+  flex:1;
+}
+
+.cart-item .cart-item-remove {
+  text-align: right;
+  cursor: pointer;
+  color: #c73f3f;
+  padding: 5px;
+}
+
+.total {
+  text-align: right;
+}
 </style>
